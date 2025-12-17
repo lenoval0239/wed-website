88de91bb85d681affe698838929c064e64e4a4c7
lenoval0239 <lenoval0239@gmail.com>
2025-12-16 21:00:29 -0600

    changed the schedule

--- src/components/RSVP.js
+++ src/components/RSVP.js
@@ -5,44 +5,42 @@ export default function RSVP() {
   return (
     <div className="rsvp">
       <div className="envelope">
-        <div className="top-container">
-          <div className="top-envelope"></div>
-          <div className="rsvp-info">
-            <div className="rsvp-content">
-              <h1>RSVP</h1>
-              <p>
-                We're beyond excited to celebrate this special day with the
-                people we care most.
-              </p>
-              <p>
-                Please let us know if you'll be joining us by clicking the RSVP
-                button on the side or sending us a message through our facebook
-                account.
-              </p>
-              <p>
-                Kindly RSVP by March 30, 2026. We can't wait to share a day full
-                of love, laughter and unforgettable memories with you!
-              </p>
-            </div>
+        <div className="top-envelope"></div>
+        <div className="rsvp-info">
+          <div className="rsvp-content">
+            <h1>RSVP</h1>
+            <p>
+              We're beyond excited to celebrate this special day with the people
+              we care most.
+            </p>
+            <p>
+              Please let us know if you'll be joining us by clicking the RSVP
+              button on the side or sending us a message through our facebook
+              account.
+            </p>
+            <p>
+              Kindly RSVP by March 30, 2026. We can't wait to share a day full
+              of love, laughter and unforgettable memories with you!
+            </p>
           </div>
         </div>
         <div className="mid-container">
           <div className="right-envelope"></div>
           <div className="left-envelope"></div>
-          <div className="bottom-envelope">
-            <button
-              id="btnRsvp"
-              onClick={() =>
-                window.open(
-                  "https://forms.gle/fr1ctdndmXhJfGfY8",
-                  "_blank",
-                  "noopener,noreferrer"
-                )
-              }
-            >
-              RSVP HERE
-            </button>
-          </div>
+        </div>
+        <div className="bottom-envelope">
+          <button
+            id="btnRsvp"
+            onClick={() =>
+              window.open(
+                "https://forms.gle/fr1ctdndmXhJfGfY8",
+                "_blank",
+                "noopener,noreferrer"
+              )
+            }
+          >
+            RSVP HERE
+          </button>
         </div>
       </div>
     </div>
